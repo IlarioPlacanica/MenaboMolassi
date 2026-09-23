a6bd29caa0719d78ca70ef5ce19e820fca40f16f
Ilario Placanica <ilario.placanica1999@gmail.com>
2026-09-23 11:31:16 +0200

    agg

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-window.MOLASSI_CONFIG = { endpoint: 'https://script.google.com/macros/s/AKfycbyKZ2CEEZbyiBNW7T2J1ESYa5xe9VoFkhYggCzWPv1W9zoA96rAIhL-lJXlJ7WFhV8/exec', pollMs: 5000 };
+window.MOLASSI_CONFIG = { endpoint: 'https://script.google.com/macros/s/AKfycbzPOMtO6uTia9xLQJe6p6YZcmD24uXTkX0QfpJl8HJFFOlCFSrw4G_bzkzegmceWlU/exec', pollMs: 5000 };
--- js/shared.js
+++ js/shared.js
@@ -4,7 +4,7 @@ window.MolassiShared = (() => {
  async function request(action, fields={}, credential=password()) {
   const endpoint=window.MOLASSI_CONFIG.endpoint;
   if(!/^https:\/\/script\.google\.com\/macros\/s\/[\w-]+\/exec$/.test(endpoint)) throw Error('Collegamento condiviso da configurare: inserire l’URL Google Apps Script in js/config.js.');
-  const response=await fetch(endpoint,{method:'POST',redirect:'follow',credentials:'omit',headers:{'Content-Type':'text/plain;charset=utf-8'},body:JSON.stringify({action,password:credential,...fields}),signal:AbortSignal.timeout(20000)});
+  const response=await fetch(endpoint,{method:'POST',redirect:'follow',credentials:'omit',headers:{'Content-Type':'text/plain;charset=utf-8'},body:JSON.stringify({action,password:credential,...fields}),signal:AbortSignal.timeout(60000)});
   if(!response.ok)throw Error('Servizio condiviso non raggiungibile.');
   const data=await response.json();
   if(!data.ok)throw Error(data.error||'Operazione non riuscita.');
