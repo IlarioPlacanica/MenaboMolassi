c816b0d337e2debf072be315afcd895a6a9c1077
Ilario Placanica <ilario.placanica1999@gmail.com>
2026-09-23 11:18:53 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-window.MOLASSI_CONFIG = { endpoint: '', pollMs: 5000 };
+window.MOLASSI_CONFIG = { endpoint: 'https://script.google.com/macros/s/AKfycbyKZ2CEEZbyiBNW7T2J1ESYa5xe9VoFkhYggCzWPv1W9zoA96rAIhL-lJXlJ7WFhV8/exec', pollMs: 5000 };
